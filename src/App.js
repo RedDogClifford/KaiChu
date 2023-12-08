e55d64fc941b8db557eefa4dde797c18900e97a2
RedDogClifford <kai.chu15@gmail.com>
2023-12-08 04:38:20 -0800

    home page start

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
-import './style.css';
+import './styles/style.css';
 
 export default function App() {
   return (
     <div>
+      <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import headerStyles from '../styles/headerStyle.module.css'
 
 export default function Header() {
   return (
-    <div>
-      <h1>header header header header</h1>
+    <div className={headerStyles.banner}>
+      <div className={headerStyles.leftGridItem}>
+        <Link to="/home" className={headerStyles.name}>KAI CHU</Link>
+      </div>
+      <div className={headerStyles.rightGridItem}>
+        <ul>
+          <li><Link to="/home">Bio</Link></li>
+          <li><Link to="/portfolio">Game Portfolio</Link></li>
+        </ul>
+      </div>
     </div>
   );
 }
